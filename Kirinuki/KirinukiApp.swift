ed78ad589ccc4e189995bab922ac9022f202db5f
ibis <cassaicus+x@gmail.com>
2025-12-01 21:50:56 +0900

    Initial Commit

---
+++ Kirinuki/KirinukiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KirinukiApp.swift
+//  Kirinuki
+//
+//  Created by ibis on 2025/12/01.
+//
+
+import SwiftUI
+
+@main
+struct KirinukiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
